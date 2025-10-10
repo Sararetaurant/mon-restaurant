84c6c90bf1f9385272138cf70b6f2ad0ce4b443f
Sararetaurant <kassimimhamed338@gmail.com>
2025-10-10 23:58:19 +0100

    fix: add commande page to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,27 +3,29 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://mon-restaurant-lac.vercel.app',
+      url: 'https://mon-restaurant-lac.vercel.app/',
       lastModified: new Date(),
-      changeFrequency: 'daily',
       priority: 1,
     },
     {
       url: 'https://mon-restaurant-lac.vercel.app/menu',
       lastModified: new Date(),
-      changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
       url: 'https://mon-restaurant-lac.vercel.app/reservation',
       lastModified: new Date(),
-      changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
       url: 'https://mon-restaurant-lac.vercel.app/contact',
       lastModified: new Date(),
-      changeFrequency: 'monthly',
+      priority: 0.7,
+    },
+    // AJOUTE LA PAGE COMMANDE !
+    {
+      url: 'https://mon-restaurant-lac.vercel.app/commande',
+      lastModified: new Date(),
       priority: 0.7,
     },
   ]
